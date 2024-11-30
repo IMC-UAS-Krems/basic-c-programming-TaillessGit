f3514041fe4e132f91860cf8e9a64912abc8cb8b
AnnaDecsov <157576481+TaillessGit@users.noreply.github.com>
2024-11-30 23:12:07 +0100

    Add files via upload

--- assignment.c
+++ assignment.c
@@ -10,9 +10,46 @@ int main(int argc, char *argv[]) {
     srand(time(NULL));
     int minrand = 1;
     int maxrand = 100;
-
-    // WRITE YOUR CODE HERE
     
+    // 1.
+    if (argc != 2){
+        printf("Incorrect usage. You provided %d arguments. The correct number of arguments is 2", argc-1);
+        return 0;
+    }
+    
+    // 2.
+    int r = argv[0]-"0";
+    int c = argv[1]-"0";
+    
+    if ((r % 1 != 0) || (c % 1 != 0) || (r < 1) || (c < 1)) {
+        printf("Incorrect usage. The parameters you provided are not positive integers");
+        return 0;
+    }
+    
+    // 3.
+    int matrix[r][c];
+    
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            matrix[i][j] = rand() % (maxrand + 1 - minrand) + minrand; //Stackoverflow
+        }
+    }
 
+    // 4.
+    FILE * mfile = fopen("matrix.txt", "w");
+    
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            fprintf(mfile,"%d",matrix[i][j]);
+            if (j != c-1) {
+                fprintf(mfile," ");
+            } else {
+                fprintf(mfile,"\n");
+            }
+        }
+    }
+
+    fclose(mfile);
+    
     return 0;
 }
